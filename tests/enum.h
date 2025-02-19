8c80049db0cc02e4f40996f1eec2de0fc17999b3
tapplencourt <tapplencourt@anl.gov>
2025-02-19 14:59:34 -0600

    run clang format

--- tests/enum.h
+++ tests/enum.h
@@ -1,10 +1,9 @@
 enum A { A0 };
 struct S0 {
-    enum B { B0 = 1} *bar;
+  enum B { B0 = 1 } * bar;
 };
 union S1 {
-    enum C { C0 = 1, C1 = 2} *bar;
+  enum C { C0 = 1, C1 = 2 } * bar;
 };
 typedef enum { D0 } D_t;
 typedef enum E { E0 } E_t;
-
--- tests/function.h
+++ tests/function.h
@@ -2,6 +2,6 @@ void f0(int);
 void f1(int b);
 void f2();
 struct a {
-   int i;
+  int i;
 };
 struct a f3(struct a x);
