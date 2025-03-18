9736093c1afd9eb00ed4bd7ffc17e1169fabb822
tapplencourt <tapplencourt@anl.gov>
2025-03-18 22:35:06 +0000

    test

--- h2yaml.py
+++ h2yaml.py
@@ -206,6 +206,8 @@ def parse_type(t: clang.cindex.Type, cursors: Callable):
     match k := t.kind:
         case _ if kind := THAPI_types.get(k):
             names = (s for s in t.spelling.split() if s not in d_qualified)
+            if kind == 'int' and 'int' not in names:
+                names += ['int']
             return {"kind": kind, "name": " ".join(names)} | d_qualified
         case clang.cindex.TypeKind.POINTER:
             return {
