58745678d4db325dc9b4594150d762551e79160a
Thomas Applencourt <tapplencourt@anl.gov>
2025-09-18 18:21:06 -0500

    Canonicalization support (#1)
    * canonicalization
    
    * add cron
    
    * move import
    
    * coverage
    
    * put pack main...
    
    * one more test

--- test_h2yaml.py
+++ test_h2yaml.py
@@ -3,7 +3,7 @@ import pytest
 from h2yaml import h2yaml
 import yaml
 
-filenames = [str(p.with_suffix("")) for p in pathlib.Path("./tests/").glob("*.yml")]
+filenames = [str(p.with_suffix("")) for p in pathlib.Path("./tests/").glob("*.h")]
 
 
 @pytest.mark.parametrize("filename", filenames)
@@ -17,10 +17,20 @@ def test_cmp_to_ref(filename):
 
 
 def test_filter_include():
-    filename = "./tests//header_filter/foo"
+    filename = "./tests/header_filter/foo"
     new_yml = yaml.safe_load(h2yaml(f"{filename}.h", pattern="foo.h"))
 
     with open(f"{filename}.yml", "r") as f:
+        ref_yml = yaml.safe_load(f)
+
+    assert new_yml == ref_yml
+
+
+def test_canonicalization():
+    filename = "./tests/function"
+    new_yml = yaml.safe_load(h2yaml(f"{filename}.h", canonicalization=True))
+
+    with open(f"{filename}_canonical.yml", "r") as f:
         ref_yml = yaml.safe_load(f)
 
     assert new_yml == ref_yml
