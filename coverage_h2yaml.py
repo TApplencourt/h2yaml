2f0cdb37939b87bd0b936b6be28932d135ec0c31
tapplencourt <tapplencourt@anl.gov>
2026-03-13 20:30:08 +0000

    refactoring

--- coverage_h2yaml.py
+++ coverage_h2yaml.py
@@ -1,29 +1,30 @@
-def clean_imports(*mod_names):
-    import sys
+from contextlib import contextmanager
+from functools import cached_property
 
-    for mod_name in mod_names:
-        sys.modules.pop(mod_name, None)
-        for key in list(sys.modules.keys()):
-            if key.startswith(mod_name + "."):
-                del sys.modules[key]
+
+@contextmanager
+def temporary_import(mod_name):
+    import sys
+    import importlib
+
+    try:
+        yield importlib.import_module(mod_name)
+    finally:
+        del sys.modules[mod_name]
 
 
 class DynamicExcludePlugin:
-    @property
+    @cached_property
     def clang_major_version(self):
-        import h2yaml
         import re
 
-        version_string = h2yaml.clang.cindex.Config.get_cindex_library_version()
-        h2yaml.clang.cindex.Config.loaded = False
-
-        clean_imports("h2yaml")
-        del h2yaml
-        match = re.search(r"version\s+(\d+)", version_string)
-        return int(match.group(1))
+        with temporary_import("h2yaml") as h2yaml:
+            version_string = h2yaml.clang.cindex.Config.get_cindex_library_version()
+            h2yaml.clang.cindex.Config.loaded = False
+            match = re.search(r"version\s+(\d+)", version_string)
+            return int(match.group(1))
 
     def configure(self, config):
-        # get_option / set_option work on BOTH Coverage and CoverageConfig
         existing = config.get_option("report:exclude_lines") or []
         if self.clang_major_version < 22:
             existing.append(r"pragma: if libclang<22: no cover")
